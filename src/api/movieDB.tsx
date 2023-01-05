e299e9627d873d53ffa6626a7ab3d8e85e87d2d9
Alexis Gilberth Renteria <gilberthr10@gmail.com>
2023-01-04 22:16:42 -0500

    Update movieDB.tsx

--- src/api/movieDB.tsx
+++ src/api/movieDB.tsx
@@ -1,6 +1,6 @@
 import  axios  from "axios";
 
-const apiKey = "3c2abc23af2f0681417a29896e2c5b6e"
+const apiKey = ""
 const movieDB = axios.create({
     baseURL: 'https://api.themoviedb.org/3/movie',
     params: {
